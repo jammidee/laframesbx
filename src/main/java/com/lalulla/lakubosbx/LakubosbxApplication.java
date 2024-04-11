13f83e927efd36f30719ed6c67b2457472cbbeba
Jammi Dee <jammi_dee@yahoo.com>
2024-04-11 19:31:11 +0800

    Updating comments

--- src/main/java/com/lalulla/lakubosbx/LakubosbxApplication.java
+++ src/main/java/com/lalulla/lakubosbx/LakubosbxApplication.java
@@ -1,3 +1,27 @@
+/**
+ * Copyright (C) 2023 Lalulla, Inc. All rights reserved.
+ * Copyright (c) 2024 - Joel M. Damaso - mailto:jammi_dee@yahoo.com Manila/Philippines
+ * This file is part of Lalulla System.
+ * 
+ * LaKuboSbx Framework is distributed under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * LaKuboSbx System is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Lalulla System.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Framework Designed by: Jammi Dee (jammi_dee@yahoo.com)
+ *
+ * File Create Date: 04/11/2024 07:28pm
+ * Created by: Jammi Dee
+ * Modified by: Jammi Dee
+ *
+*/
+
 package com.lalulla.lakubosbx;
 
 import java.util.Arrays;
--- src/main/java/com/lalulla/lakubosbx/MainWebController.java
+++ src/main/java/com/lalulla/lakubosbx/MainWebController.java
@@ -24,14 +24,13 @@
 
 package com.lalulla.lakubosbx;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootTest
-class LakubosbxApplicationTests {
-
-	@Test
-	void contextLoads() {
+@RestController
+public class MainWebController {
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
 	}
-
 }
