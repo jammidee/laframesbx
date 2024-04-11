3fe388c109d0aa70a276d7e79ab207be3d4c9038
Jammi Dee <jammi_dee@yahoo.com>
2024-04-11 22:33:06 +0800

    Added ANSI color.

--- src/main/java/com/lalulla/lakubosbx/LakubosbxApplication.java
+++ src/main/java/com/lalulla/lakubosbx/LakubosbxApplication.java
@@ -35,6 +35,12 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class LakubosbxApplication {
 
+    // ANSI escape codes for colors
+    private static final String ANSI_COLOR_RESET 	= "\u001B[0m";
+    //private static final String ANSI_COLOR_RED 		= "\u001B[31m";
+    private static final String ANSI_COLOR_GREEN 	= "\u001B[32m";
+    //private static final String ANSI_COLOR_YELLOW 	= "\u001B[33m";
+
 	public static void main(String[] args) {
 		SpringApplication.run(LakubosbxApplication.class, args);
 	}
@@ -44,7 +50,7 @@ public class LakubosbxApplication {
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
 
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
+			System.out.printf("\n\n%sLet's inspect the beans provided by Spring Boot:%s\n\n", ANSI_COLOR_GREEN, ANSI_COLOR_RESET);
 
 			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
