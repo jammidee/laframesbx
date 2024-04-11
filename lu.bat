4f6dc86bfefff4e845324c0dbb596f208a649993
Jammi Dee <jammi_dee@yahoo.com>
2024-04-11 19:20:41 +0800

    Initial commit Lalulla Framework Spring-boot with web, security and Ollama

---
+++ lu.bat
@@ -0,0 +1,2 @@
+./mvnw clean package
+java -jar target/boiler-web-0.1.0.jar
---
+++ src/test/java/com/lalulla/lakubosbx/LakubosbxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lalulla.lakubosbx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakubosbxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
